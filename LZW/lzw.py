8039d4f8459fb2760e88b151aec0d5ecdd8f951c
Dmytro-Mld <79337586+Dmytro-Mld@users.noreply.github.com>
2025-12-19 01:45:01 +0200

    LZW: add vocabulary restrictions

--- LZW/lzw.py
+++ LZW/lzw.py
@@ -1,10 +1,36 @@
-def lzw_compress(data: bytes):
+class BitWriter:
+    def __init__(self):
+        self.buffer = 0
+        self.bit_count = 0
+        self.out = bytearray()
 
+    def write(self, value, bits):
+        self.buffer |= value << self.bit_count
+        self.bit_count += bits
+
+        while self.bit_count >= 8:
+            self.out.append(self.buffer & 0xFF)
+            self.buffer >>= 8
+            self.bit_count -= 8
+
+    def flush(self):
+        if self.bit_count > 0:
+            self.out.append(self.buffer & 0xFF)
+
+        return self.out
+
+
+def lzw_compress(data: bytes):
+    MAX_CODE = 4095
+    CODE_WIDTH = 12
+
+    # 1. Ініціалізація словника
     voc = {bytes([i]): i for i in range(256)}
     next_code = 256
 
+    writer = BitWriter()
+
     s = b""
-    output = []
 
     for byte in data:
         c = bytes([byte])
@@ -13,13 +39,19 @@ def lzw_compress(data: bytes):
         if sc in voc:
             s = sc
         else:
-            output.append(voc[s])
-            voc[sc] = next_code
-            next_code += 1
+            writer.write(voc[s], CODE_WIDTH)
+
+            if next_code <= MAX_CODE:
+                voc[sc] = next_code
+                next_code += 1
+
             s = c
 
     if s:
-        output.append(voc[s])
+        writer.write(voc[s], CODE_WIDTH)
+
+    return writer.flush()
+
 
 
 if __name__ == "__main__":
