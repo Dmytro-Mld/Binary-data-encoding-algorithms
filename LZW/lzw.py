ef7dc6d91e0d875661e08fb354a8aefdd40e03b0
Dmytro-Mld <79337586+Dmytro-Mld@users.noreply.github.com>
2025-12-19 01:22:06 +0200

    LZW: add compression function

--- LZW/lzw.py
+++ LZW/lzw.py
@@ -1,7 +1,27 @@
-def lzw():
-        with open(r"C:\Users\dmmol\OneDrive\Documents\GitHub\Binary-data-encoding-algorithms\LZW\Exe files\whois64.exe", "rb") as f:
-            data = f.read()
-        print(data[:1000])
+def lzw_compress(data: bytes):
+
+    voc = {bytes([i]): i for i in range(256)}
+    next_code = 256
+
+    s = b""
+    output = []
+
+    for byte in data:
+        c = bytes([byte])
+        sc = s + c
+
+        if sc in voc:
+            s = sc
+        else:
+            output.append(voc[s])
+            voc[sc] = next_code
+            next_code += 1
+            s = c
+
+    if s:
+        output.append(voc[s])
+
 
 if __name__ == "__main__":
-    lzw()
+    with open(r"C:\Users\dmmol\OneDrive\Documents\GitHub\Binary-data-encoding-algorithms\LZW\Exe files\whois64.exe", "rb") as f:
+        data = f.read()
