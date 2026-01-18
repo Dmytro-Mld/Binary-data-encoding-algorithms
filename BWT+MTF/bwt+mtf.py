4d704c0893ea45140e51c455ddd1fa385862f640
Dmytro-Mld <79337586+Dmytro-Mld@users.noreply.github.com>
2026-01-18 23:48:33 +0200

    BTW: implementing suffix array + radix sort

--- BWT+MTF/bwt+mtf.py
+++ BWT+MTF/bwt+mtf.py
@@ -6,6 +6,38 @@ def matrix(input):
     for i in input:
         pass
     
+    
+
+def build_suffix_array(data: bytes):
+    n = len(data)
+
+    sa = list(range(n))
+    rank = list(data)
+
+    k = 1
+    tmp = [0] * n
+
+    while k < n:
+
+        sa.sort(key=lambda i: (rank[i], rank[i + k] if i + k < n else -1))
+
+        tmp[sa[0]] = 0
+        for i in range(1, n):
+            prev = sa[i - 1]
+            curr = sa[i]
+            tmp[curr] = tmp[prev] + (
+                (rank[prev], rank[prev + k] if prev + k < n else -1) <
+                (rank[curr], rank[curr + k] if curr + k < n else -1)
+            )
+
+        rank = tmp[:]
+        if max(rank) == n - 1:
+            break
+
+        k <<= 1
+
+    return sa
+    
 
 def bwt():
     matrix(test)
