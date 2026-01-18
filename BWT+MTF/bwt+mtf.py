e813bb73a368045c0e81d8f6d91e1abf27addb7d
Dmytro-Mld <79337586+Dmytro-Mld@users.noreply.github.com>
2026-01-18 22:54:22 +0200

    BTW: Create code structure

---
+++ BWT+MTF/bwt+mtf.py
@@ -0,0 +1,14 @@
+test = bytes("abracadabra", "utf-8")
+print(test)
+
+def matrix(input):
+    mtrx = []
+    for i in input:
+        pass
+    
+
+def bwt():
+    matrix(test)
+
+if __name__ == "__main__":
+    bwt()
